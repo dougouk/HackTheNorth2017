3b93a512753ec945ab8d91378945615bb973ce55
David Luo <y72luo@uwaterloo.ca>
2017-09-16 20:31:09 -0400

    small change

--- Android/app/src/main/java/com/dan190/covfefe/Util/GroupUtils.java
+++ Android/app/src/main/java/com/dan190/covfefe/Util/GroupUtils.java
@@ -2,6 +2,7 @@ package com.dan190.covfefe.Util;
 
 import android.app.Activity;
 
+import com.dan190.covfefe.Models.User;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -14,12 +15,12 @@ public class GroupUtils extends Activity {
     MyAppApplication mApp = ((MyAppApplication)getApplicationContext());
     FirebaseDatabase database = mApp.getGlobalDB();
 
-    public String init_user(String user_name){
+    public String init_user(User new_user){
 
         DatabaseReference usersRef = database.getReference("users");
 
         DatabaseReference newUserRef = usersRef.push();
-        newUserRef.setValue(new Structure.User(user_name));
+        newUserRef.setValue(new_user);
 
         String user_id = newUserRef.getKey();
 
@@ -31,7 +32,10 @@ public class GroupUtils extends Activity {
     }
 
     public void start_group(String group_name, int user_id){
+//        DatabaseReference groupRef = database.getReference("groups");
 
+//        DatabaseReference newGroupRef = groupRef.push();
+//        newGroupRef.setValue(new )
     }
 
     public void join_group(int group_id, int user_id){
